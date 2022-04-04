dd943096d3b10b54f827d0324311de6b04a5625b
Ennin Jeter <jeterennin@gmail.com>
2022-04-04 17:04:23 -0500

    updated settings + urls + models files

--- supers/models.py
+++ supers/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from super_types.models import Super_Types
+
 # Create your models here.
 
 class Super(models.Model):
@@ -8,5 +10,5 @@ class Super(models.Model):
     primary_ability = models.CharField(max_length=255)
     secondary_ability = models.CharField(max_length=255)
     catchphrase = models.CharField(max_length=255)
-    super_type = models.ForeignKey
-    
+    super_type = models.ForeignKey(Super_Types, on_delete=models.CASCADE)
+
