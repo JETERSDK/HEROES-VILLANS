a411840beb0e8dadde35d85162ad5a4f5ce14333
Ennin Jeter <jeterennin@gmail.com>
2022-04-05 14:20:36 -0500

    Modified admin/serializers/views

--- super_types/serializers.py
+++ super_types/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Super_Types
 
 
-class SuperSerializer(serializers.ModelSerializer):
+class Super_TypesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Super_Types
         fields = ['id', 'type']
--- super_types/views.py
+++ super_types/views.py
@@ -1,9 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import Super_TypesSerializer
+from .models import Super_Types
 
 @api_view(['GET'])
 def super_types_list(request):
+    super_type = Super_Types.objects.all()
+
+    serializer = Super_TypesSerializer(super_type, many=True)
 
 
 
-    return Response('ok!')
+    return Response(serializer.data)
